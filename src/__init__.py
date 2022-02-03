b56d6d7c1ebcbdf3c9e4976b33e1279ec6105364
Hains <hainsvdbosch@ziggo.nl>
2022-02-03 10:31:35 +0100

    Update to be more compatible with python 3
    -Use print() function.
    -Update raise syntax.

--- src/__init__.py
+++ src/__init__.py
@@ -14,7 +14,7 @@ def localeInit():
 def _(txt):
 	t = gettext.dgettext("SundtekControlCenter", txt)
 	if t == txt:
-		#print "[SundtekControlCenter] fallback to default translation for", txt
+		#print("[SundtekControlCenter] fallback to default translation for", txt)
 		t = gettext.gettext(txt)
 	return t
 
